66b8dde854f628d8820b728e645782fe50673327
montsea999 <montsea999@gmail.com>
2022-11-23 22:49:57 +0100

    committing all changes

---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,58 @@
+import { TiempoService } from './../../services/tiempo.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
+})
+export class DashboardComponent implements OnInit {
+  // png obtenido desde el site https://www.freeiconspng.com/images/weather-icon-png
+ // srcImgDashboard ='https://www.pngall.com/wp-content/uploads/11/Weather-No-Background.png';
+  ciudad = '';
+  temperaturaMax = 0;
+  temperaturaMin = 0;
+  velViento = 0;
+  dirViento = 0;
+  humedad = 0;
+  presion=0;
+  tiempo='';
+  query= false;   //inicialmente falso para que no se renderice la tarjeta con los datos recibidos de la API
+  loading = false; //inicialmente falso para que no se renderice el spinner
+  error = false;
+  constructor(private _tiempoService:TiempoService) {}
+
+  ngOnInit(): void {}
+
+  getTiempo() {
+    this.query=false;  //false para que no se renderice la tarjeta para mostrar los datos todavía
+    this.loading = true;  //true para que se renderice el spinner mientras se gestione la petición http
+
+    this._tiempoService.getAPIdata(this.ciudad).subscribe(data=>{
+      console.log(data.main);
+      this.loading = false;  // false para que deje de renderizarse el spinner
+      this.query = true;//true para que se visualice la tarjeta para mostrar los datos obtenidos
+
+      this.temperaturaMax = data.main.temp_max - 273.15;  //paso los grados kelvin a celsius restando 273.15
+      this.temperaturaMin = data.main.temp_min - 273.15;
+      this.velViento = data.wind.speed;
+      this.dirViento = data.wind.deg;
+
+      this.presion = data.main.pressure;
+      this.tiempo = data.weather[0].main;
+      //si cambiamos icon por main podremos mostrar el icono correspondiente al tiempo (habría que crear
+      //un array de imagenes y setear el src segun codigo recibido de la API)
+
+    }, error =>{
+      this.loading=false;
+      this.setError();
+    })
+  }
+  setError(){
+    this.error = true;
+    setTimeout(() => {
+      this.error = false;
+      this.ciudad='';
+    }, 3000);
+  }
+}
---
+++ src/app/services/tiempo.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TiempoService {
+  //https://openweathermap.org
+  //mi Api call según la API, para hacer una llamada por ciudad es 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}'
+  //cambio la url quitando q={city name} y {API key} ya que los concatenaré luego
+  //ojo para subirlo a un servidor he cambiado http por https para que la request sea segura
+  url = 'https://api.openweathermap.org/data/2.5/weather?q=';
+  key = '0f1c461a4102be122e87e1ef426e500c';
+
+  //inyecto la clase HttpClient y VSC la importa
+  constructor(private http: HttpClient) {}
+
+  //creo el metodo que recibira la ciudad por parametro y devolvera un Observable de tipo any
+  //los c se suscribiran a este metodo (una vez inyectado el servicio) que devuelve un observable y así los c podran obtener los D de forma asincrona
+  //creo la constante URL concatenando url (sin q={city name} ni {API key}) + {API key} + '&q='+ ciudad (variable alimentada por el banana in box)
+
+  getAPIdata(ciudad: string): Observable<any> {
+    const URL = this.url + ciudad + '&lang=es' + '&appid=' + this.key ;
+
+    return this.http.get(URL);
+  }
+}
